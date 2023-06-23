9ec6643b7543b3812cb52de532b08e19fc944f2e
gaohan1994 <871418277@qq.com>
2023-06-23 15:48:52 +0800

    Update sliding window solution

--- leetcode/sliding-window.js
+++ leetcode/sliding-window.js
@@ -107,3 +107,133 @@ function slidingWindow(s, t) {
   const str = minWindow("ADOBECODEBANC", "ABC");
   console.log("str", str);
 }
+
+{
+  // https://leetcode.cn/problems/permutation-in-string/
+  // 567 题
+  // 给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的排列。
+  // 如果是，返回 true ；否则，返回 false 。
+  // 换句话说，s1 的排列之一是 s2 的 子串 。
+
+  /**
+   * @param {string} s1
+   * @param {string} s2
+   * @return {boolean}
+   */
+  var checkInclusion = function (s1, s2) {
+    let need = new Map();
+    let box = new Map();
+
+    for (let c of s1) {
+      need.set(c, (need.get(c) ?? 0) + 1);
+    }
+
+    let left = 0,
+      right = 0,
+      valid = 0;
+    // length = Infinity;
+
+    while (right < s2.length) {
+      const char = s2[right];
+      right++;
+
+      console.log(`left: ${left}, right: ${right}`);
+
+      if (need.has(char)) {
+        box.set(char, (box.get(char) ?? 0) + 1);
+
+        if (need.get(char) === box.get(char)) {
+          valid++;
+        }
+      }
+
+      while (valid === need.size) {
+        // 缩小窗口
+
+        if (right - left === s1.length) {
+          // 如果窗口内的字符串刚好等于子串长度则说明包含子串
+          return true;
+        }
+
+        const deleteChar = s2[left];
+        left++;
+
+        if (need.has(deleteChar)) {
+          if (box.get(deleteChar) === need.get(deleteChar)) {
+            valid--;
+          }
+          box.set(deleteChar, (box.get(deleteChar) ?? 0) - 1);
+        }
+      }
+    }
+
+    return false;
+  };
+}
+
+{
+  // 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。
+  // 不考虑答案输出的顺序。
+  // 异位词 指由相同字母重排列形成的字符串（包括相同的字符串）。
+  // 来源：力扣（LeetCode） 438 题
+  // 链接：https://leetcode.cn/problems/find-all-anagrams-in-a-string
+
+  // 输入: s = "cbaebabacd", p = "abc"
+  // 输出: [0,6]
+  // 解释:
+  // 起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+  // 起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+
+  //   输入: s = "abab", p = "ab"
+  // 输出: [0,1,2]
+  // 解释:
+  // 起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+  // 起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+  // 起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+
+  var findAnagrams = function (s, p) {
+    const need = new Map();
+    const box = new Map();
+
+    for (let c of p) {
+      need.set(c, (need.get(c) ?? 0) + 1);
+    }
+
+    let left = 0,
+      right = 0,
+      valid = 0,
+      result = [];
+
+    while (right < s.length) {
+      const char = s[right];
+      right++;
+
+      if (need.has(char)) {
+        box.set(char, (box.get(char) ?? 0) + 1);
+
+        if (need.get(char) === box.get(char)) {
+          valid++;
+        }
+      }
+
+      while (valid === need.size) {
+        if (right - left === p.length) {
+          result.push(left);
+        }
+
+        const deleteChar = s[left];
+        left++;
+
+        if (need.has(deleteChar)) {
+          if (box.get(deleteChar) === need.get(deleteChar)) {
+            valid--;
+          }
+
+          box.set(deleteChar, (box.get(deleteChar) ?? 0) - 1);
+        }
+      }
+    }
+
+    return result;
+  };
+}
